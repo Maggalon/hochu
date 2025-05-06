69f57c25883fe1c2b8cd8512028882b30ef61809
Maggalon <abstergo22h@gmail.com>
2025-05-06 11:21:37 +1000

    Trying TG coloring

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,6 @@ export default {
       colors: {
         // background: "var(--background)",
         // foreground: "var(--foreground)",
-        button: "var(--tg-theme-button-color)",
       },
     },
   },
